a8fd42ac4b5fad249286db617e8034007a728176
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-06-08 07:37:22 -0400

    implemented react-router and set up navigation page

---
+++ src/routes/navigation/Navigation.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { RiShoppingBagLine } from "react-icons/ri";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <CrwnLogo className="logo" />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="/shop">
+                        SHOP
+                    </Link>
+                    <Link className="nav-link" to="/contact">
+                        CONTACT
+                    </Link>
+                    <Link className="nav-link" to="/sigin">
+                        SIGN IN
+                    </Link>
+                    <RiShoppingBagLine className="nav-link-cart" size={28} />
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
+};
+
+export default Navigation;
