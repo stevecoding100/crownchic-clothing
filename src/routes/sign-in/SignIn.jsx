225048a44e663557c3658e99343e1ecb7c970812
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-06-09 03:31:35 -0400

    Created sign up component, the form input component, button component

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,19 +1,41 @@
 import React from "react";
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 import {
+    // auth,
     signInWithGooglePopup,
+    // signInWithGoogleRedirect,
     createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/SignUpForm";
+
 const SignIn = () => {
+    // useEffect(() => {
+    //     const fetchRedirectResult = async () => {
+    //         const response = await getRedirectResult(auth);
+
+    //         if (response) {
+    //             console.log(response);
+    //             const userDocRef = await createUserDocumentFromAuth(
+    //                 response.user
+    //             );
+    //         }
+    //     };
+
+    //     fetchRedirectResult();
+    // }, []);
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     };
+
     return (
         <div>
             <h1>Sign In Page</h1>
-
-            <button onClick={logGoogleUser}>Sign in with Google</button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <SignUpForm />
         </div>
     );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
 import {
     getAuth,
-    signInWithRedirect,
+    // signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
+    createUserWithEmailAndPassword,
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -21,19 +22,28 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
     prompt: "select_account",
 });
 
 export const auth = getAuth();
 
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+    signInWithPopup(auth, googleProvider);
+
+// export const signInWithGoogleRedirect = () =>
+//     signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+    userAuth,
+    additionalInformation = {}
+) => {
+    if (!userAuth) return;
+
     const userDocRef = doc(db, "users", userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
 
@@ -47,6 +57,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
                 displayName,
                 email,
                 createdAt,
+                ...additionalInformation,
             });
         } catch (error) {
             console.log("Errro creating the user", error.message);
@@ -54,4 +65,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     }
     // if user data exists
     return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+    return await createUserWithEmailAndPassword(auth, email, password);
 };
