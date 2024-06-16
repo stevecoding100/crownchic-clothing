7addc7b002b40c9e2692e2fd372feaacfd8f9dab
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-06-16 18:01:13 -0400

    installed styled components framework

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -11,7 +11,12 @@ import { CartContext } from "../../context/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import "./navigation.styles.scss";
+import {
+    NavigationContainer,
+    NavLinks,
+    NavLink,
+    LogoContainer,
+} from "./navigation.styles";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
@@ -19,30 +24,24 @@ const Navigation = () => {
 
     return (
         <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to="/">
+            <NavigationContainer>
+                <LogoContainer to="/">
                     <CrwnLogo className="logo" />
-                </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-link" to="/shop">
-                        SHOP
-                    </Link>
-                    <Link className="nav-link" to="/contact">
-                        CONTACT
-                    </Link>
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to="/shop">SHOP</NavLink>
+                    <NavLink to="/contact">CONTACT</NavLink>
                     {currentUser ? (
-                        <span className="nav-link" onClick={signOutUser}>
+                        <NavLink as="span" onClick={signOutUser}>
                             SIGN OUT
-                        </span>
+                        </NavLink>
                     ) : (
-                        <Link className="nav-link" to="/auth">
-                            SIGN IN
-                        </Link>
+                        <NavLink to="/auth">SIGN IN</NavLink>
                     )}
                     <CartIcon />
-                </div>
+                </NavLinks>
                 {isCartOpen && <CartDropdown />}
-            </div>
+            </NavigationContainer>
             <Outlet />
         </Fragment>
     );
---
+++ src/routes/navigation/navigation.styles.jsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const NavigationContainer = styled.div`
+    height: 70px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 25px;
+
+    @media (max-width: 600px) {
+        height: 60px;
+        margin-bottom: 20px;
+    }
+`;
+
+export const LogoContainer = styled(Link)`
+    height: 100%;
+    width: 70px;
+    padding: 25px;
+    cursor: pointer;
+
+    @media (max-width: 600px) {
+        width: 50px;
+        padding: 15px;
+    }
+`;
+
+export const NavLinks = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    @media (max-width: 600px) {
+        width: 80%;
+    }
+`;
+
+export const NavLink = styled(Link)`
+    margin-top: 30px;
+    padding: 10px 15px;
+    font-weight: 500;
+    cursor: pointer;
+
+    @media (max-width: 600px) {
+        padding: 10px;
+        font-weight: 600;
+        font-size: 14px;
+    }
+`;
+
+// @media only screen and (max-width: 600px) {
+//     .navigation {
+//         .nav-links-container {
+//             .nav-link {
+//                 padding: 10px;
+//                 font-weight: 600;
+//                 font-size: 14px;
+//                 cursor: pointer;
+//             }
+//         }
+//     }
+// }
