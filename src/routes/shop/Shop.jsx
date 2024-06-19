7aba3f374d5c68323a79e0df6bb7530be1de43f5
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-06-19 19:57:45 -0400

    deleted unused import

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 
 import CategoriesPreview from "../categories-preview/CategoriesPreview";
 import Category from "../category/Category";
-import "./shop.styles.scss";
 
 const Shop = () => {
     return (
