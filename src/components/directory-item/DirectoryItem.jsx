a7fbef12a1467c6ca23d30683b62bdd0d791dd39
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-06-20 11:01:15 -0400

    updated style for directory component and Home.jsx

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import {
     BackgroundImage,
     Body,
@@ -7,9 +7,12 @@ import {
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             <BackgroundImage imageUrl={imageUrl} />
             <Body>
                 <h2>{title}</h2>
--- src/components/directory/directory.styles.jsx
+++ src/components/directory/directory.styles.jsx
@@ -5,4 +5,18 @@ export const DirectoryContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
+    gap: 20px;
+    @media screen and (max-width: 1024px) {
+        gap: 15px; /* Adjust gap for medium-sized screens */
+    }
+
+    @media screen and (max-width: 768px) {
+        justify-content: center;
+        gap: 10px;
+    }
+
+    @media screen and (max-width: 480px) {
+        flex-direction: column;
+        gap: 5px;
+    }
 `;
